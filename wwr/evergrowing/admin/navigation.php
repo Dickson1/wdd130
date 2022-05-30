f810544bbf973b3bbdb1cf1bbd0fe07f264bcb7d
Dickson1 <64061194+Dickson1@users.noreply.github.com>
2022-05-30 15:45:20 +0300

    Update navigation.php

--- wwr/evergrowing/admin/navigation.php
+++ wwr/evergrowing/admin/navigation.php
@@ -13,17 +13,17 @@
       <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav m-auto">
               <li class="nav-item active">
-                  <a class="nav-link" href="../#index.php">Home <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="../#index.html">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                  <a class="nav-link" href="about.php">About</a>
+                  <a class="nav-link" href="about.html">About</a>
               </li>
               
               <li class="nav-item1">
-                  <a class="nav-link" href="product.php">Products</a>
+                  <a class="nav-link" href="product.html">Products</a>
               </li>
               <li class="nav-item">
-                  <a class="nav-link" href="../#contact.php">Contact</a>
+                  <a class="nav-link" href="../#contact.html">Contact</a>
               </li>
           </ul>
           <ul class="navbar-nav">
